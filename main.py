15000f9b990994f1ad11e267c372826a359cebec
Markmanus <pampadalampa@gmail.com>
2023-07-24 21:21:09 +0100

    added text.

--- main.py
+++ main.py
@@ -18,5 +18,24 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+canvas = Canvas(width=220, height=225 ,bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(103,113,image=tomato_img)
+canvas.create_text(103,130,text="00:00",fill="white",font=(FONT_NAME,35,"bold"))
+canvas.grid(column=1,row=1)
+
+text = Label(text="Timer",fg=GREEN,bg=YELLOW,font=(FONT_NAME,50,"bold"))
+text.grid(column=1,row=0)
+
+button_start = Button(text="Start",highlightthickness=0)
+button_start.grid(column=0,row=2)
+
+button_reset = Button(text="Reset",highlightthickness=0)
+button_reset.grid(column=2,row=2)
+
+check_mark = Label(text="✔",fg=GREEN,bg=YELLOW,font=(FONT_NAME,20,"bold"))
+
+
 
 window.mainloop()
